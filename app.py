a3f6a31ec39d58104a43779868c6b25e0507c052
shankars25 <sutarshankar420@gmail.com>
2025-02-14 08:24:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -178,6 +178,6 @@ def get_files():
 if __name__ == "__main__":
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=10000,)
 
 
